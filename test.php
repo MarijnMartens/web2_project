204e93c345538110201ec6264c461feed5b11318
Marijn <Marijn@Kreplox>
2013-12-28 19:31:53 +0100

    Test database live

--- test.php
+++ test.php
@@ -6,23 +6,15 @@
     </head>
     <body style="background-color: lime;">
         <?php
-        // put your code here
+        $this->db->where('username', 'maarten');
+        $query = $this->db->get('users');
+            $row = $query->row();
+                    $username = $row->username;
+                    $email = $row->email;
+                
+        echo 'username is ' . $username . '<br/>';
+        echo 'email is ' . $email . '<br/>';
+        
         ?>
-        <a class="twitter-timeline" 
-           width="200"
-           height="500"
-           data-link-color="#1c8017"
-           data-chrome="nofooter noscrollbar"
-           href="https://twitter.com/Marijn_Martens"  
-           data-widget-id="416921452827275264">Tweets by @Marijn_Martens</a>
-        <script>!function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
-            if (!d.getElementById(id)) {
-                js = d.createElement(s);
-                js.id = id;
-                js.src = p + "://platform.twitter.com/widgets.js";
-                fjs.parentNode.insertBefore(js, fjs);
-            }
-        }(document, "script", "twitter-wjs");</script>
     </body>
 </html>
