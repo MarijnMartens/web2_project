8a11c6fc0a62af4977664da5be6d8e8bc733b8f7
Marijn <Marijn@Kreplox>
2013-12-31 13:26:23 +0100

    Test Facebook Login

---
+++ application/config/facebook.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['appID'] = "215681628615224";
+$config['appSecret'] = "944b46222759f0516bfb697762fe4041";
---
+++ test2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>TEST2.PHP</h1>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
