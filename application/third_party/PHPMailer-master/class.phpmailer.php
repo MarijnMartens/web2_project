5ec226c3525d45f6d6e56ca69d19d631c51df5b0
Marijn <Marijn@Kreplox>
2014-01-09 00:21:55 +0100

    Password_reset aangepast Bug geplet, email framework Bug opgedoken bij verkeerd invullen gegevens

--- application/third_party/PHPMailer-master/class.phpmailer.php
+++ application/third_party/PHPMailer-master/class.phpmailer.php
@@ -84,7 +84,7 @@ class PHPMailer
      * The From name of the message.
      * @type string
      */
-    public $FromName = 'Root User';
+    public $FromName = '';
 
     /**
      * The Sender email (Return-Path) of the message.
