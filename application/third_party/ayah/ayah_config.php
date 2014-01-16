7c2680d6e13945c51c1321cb9d07abd56b22c1f2
Marijn <Marijn@Kreplox>
2014-01-16 14:48:21 +0100

    captcha test

---
+++ application/third_party/ayah/ayah_config.php
@@ -0,0 +1,13 @@
+<?php
+// Edit the two lines below to use the keys for your site.
+// (Note: you can find your keys at http://portal.areyouahuman.com/dashboard)
+define( 'AYAH_PUBLISHER_KEY', 'd66d760fb64326974cc172696cc777c8f30254fd');
+define( 'AYAH_SCORING_KEY', 'ccc3ce3c8676ed38d2dff978f892da7d91a06595');
+
+
+// Set defaults for values needed by the ayah.php file.
+// (Note: you do not need to change these.)
+define( 'AYAH_WEB_SERVICE_HOST', 'ws.areyouahuman.com');
+define( 'AYAH_TIMEOUT', 0);
+define( 'AYAH_DEBUG_MODE', FALSE);
+define( 'AYAH_USE_CURL', TRUE);
